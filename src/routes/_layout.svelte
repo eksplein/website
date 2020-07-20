98547ec049266530ba1fc86cd9afd1b28cf3e087
tommywalkie <tommywalkie@gmail.com>
2020-07-20 18:44:40 +0200

    Mentionned license in most source files and set up Docma

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,3 +1,22 @@
+<!--
+  EKSPLEIN (/ɛkˈspleɪn/) is a simple and stupid glossary-like blog
+  in which things are explained 
+  Copyright (C) 2020  Tom Bazarnik and the contributors
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
 <script>
   import Header from '../components/Header.svelte'
   export let segment
@@ -34,6 +53,13 @@
     text-align: center;
     width: 100%;
   }
+
+  .license {
+    vertical-align: sub;
+    height: 22px;
+    filter: grayscale(1);
+    margin-right: 8px;
+  }
 </style>
 
 <div class="layout">
@@ -44,6 +70,7 @@
   </main>
 
   <footer>
+    <a target="_blank" href="http://www.gnu.org/licenses/gpl-3.0.en.html"><img class="license" alt="GNU General Public License v3.0" src="https://www.gnu.org/graphics/gplv3-88x31.png" /></a>
     <span>
       &copy; {new Date().getFullYear()} Tom Bazarnik and the contributors.
     </span>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,22 @@
+<!--
+  EKSPLEIN (/ɛkˈspleɪn/) is a simple and stupid glossary-like blog
+  in which things are explained 
+  Copyright (C) 2020  Tom Bazarnik and the contributors
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
 <style>
   .home-container {
     align-items: center;
@@ -10,6 +29,10 @@
 
   .home-copy {
     flex: 1;
+  }
+
+  .home-copy h1 {
+    text-align: center;
   }
 
   h1 {
@@ -55,7 +78,6 @@
     .home-copy {
       flex: 0;
       padding-bottom: 2em;
-      text-align: center;
     }
   }
 </style>
